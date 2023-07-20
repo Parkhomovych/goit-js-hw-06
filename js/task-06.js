0aca1baf51991c5fc7b898582ad9bc94007a7ab9
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-07-20 11:41:40 +0300

    fix bag

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,9 @@ const input = document.querySelector('#validation-input');
 console.dir(input.id);
 const length = input.dataset.length;
 input.addEventListener('blur', elem => {
-  if (elem.currentTarget.value.length > length) {
-    input.classList = 'invalid';
-  } else if (elem.currentTarget.value.length < length) {
+  if (elem.currentTarget.value.length === Number(length)) {
     input.classList = 'valid';
+  } else {
+    input.classList = 'invalid';
   }
 });
